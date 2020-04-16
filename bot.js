5503a094dd3f636fea050426d0194d8b620a8c90
fardado90 <55768801+fardado90@users.noreply.github.com>
2020-04-16 18:12:54 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,9 +15,40 @@ client.on('message', message => {
 	    	}
     		if(carros[i].trim() ==='Exemplar'){
 	 			message.channel.send('Exemplar:');
-				message.channel.send('https://wiki.rage.mp/images/thumb/a/a4/Exemplar.png/800px-Exemplar.png');
+				message.channel.send('');
 	 		}
-	 i++;	
+	 		//----------------------------------COMPACTS---------------------------------------------------------
+	 		if(carros[i].trim() ==='Blista'){
+	 			message.channel.send('Blista:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/5/58/Blista.png/800px-Blista.png');
+	 		}
+	 	
+	 		if(carros[i].trim() ==='Brioso'){
+	 			message.channel.send('Brioso:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/6/6f/Brioso.png/800px-Brioso.png');
+	 		}
+	 		if(carros[i].trim() ==='Dilettante'){
+	 			message.channel.send('Dilettante:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/b/b9/Dilettante.png/800px-Dilettante.png');
+	 		}
+	 		if(carros[i].trim() ==='Issi2'){
+	 			message.channel.send('Issi2:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/0/0b/Issi2.png/800px-Issi2.png');
+	 		}
+	 		if(carros[i].trim() ==='Panto'){
+	 			message.channel.send('Panto:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/e/e5/Panto.png/800px-Panto.png');
+	 		}
+	 		if(carros[i].trim() ==='Prairie'){
+	 			message.channel.send('Prairie:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/3/3d/Prairie.png/800px-Prairie.png');
+	 		}
+	 		if(carros[i].trim() ==='Rhapsody'){
+	 			message.channel.send('Rhapsody:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/e/e2/Rhapsody.png/800px-Rhapsody.png');
+	 		}
+
+	i++;	
 	 }
 	 
 	
