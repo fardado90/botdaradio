1a229a31f4f0e970a4b1d0eb8cbb2e8188a6cb78
fardado90 <55768801+fardado90@users.noreply.github.com>
2020-04-16 18:20:16 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,15 +9,8 @@ client.on('message', message => {
 	 var carros = message.content.split(',');
 	 var i = 0;
 	 while (i < carros.length) {
-    		if (carros[i].trim() === 'f620') {
-    			message.channel.send('f620:');
-				message.channel.send('https://wiki.rage.mp/images/thumb/2/21/F620.png/800px-F620.png');
-	    	}
-    		if(carros[i].trim() ==='Exemplar'){
-	 			message.channel.send('Exemplar:');
-				message.channel.send('');
-	 		}
-	 		//----------------------------------COMPACTS---------------------------------------------------------
+    		
+    		//----------------------------------COMPACTS---------------------------------------------------------
 	 		if(carros[i].trim() ==='Blista'){
 	 			message.channel.send('Blista:');
 				message.channel.send('https://wiki.rage.mp/images/thumb/5/58/Blista.png/800px-Blista.png');
@@ -47,9 +40,35 @@ client.on('message', message => {
 	 			message.channel.send('Rhapsody:');
 				message.channel.send('https://wiki.rage.mp/images/thumb/e/e2/Rhapsody.png/800px-Rhapsody.png');
 	 		}
+	 		//------------------------------------Coupes-------------------------------------------------------------
+	 		if(carros[i].trim() ==='CogCabrio'){
+	 			message.channel.send('CogCabrio:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/1/1b/CogCabrio.png/800px-CogCabrio.png');
+	 		}
+	 		if(carros[i].trim() ==='Exemplar'){
+	 			message.channel.send('Exemplar:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/a/a4/Exemplar.png/800px-Exemplar.png');
+	 		}
+	 		if(carros[i].trim() ==='Exemplar'){
+	 			message.channel.send('Exemplar:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/a/a4/Exemplar.png/800px-Exemplar.png');
+	 		}
+	 		if (carros[i].trim() === 'F620') {
+    			message.channel.send('F620:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/2/21/F620.png/800px-F620.png');
+	    	}
+	    	if (carros[i].trim() === 'Felon') {
+    			message.channel.send('Felon:');
+				message.channel.send('https://wiki.rage.mp/images/thumb/0/04/Felon.png/800px-Felon.png');
+	    	}
+	    	
+	    	if (carros[i].trim() === 'Felon2') {
+    			message.channel.send('Felon2:\nhttps://wiki.rage.mp/images/thumb/6/6e/Felon2.png/800px-Felon2.png');
+			}
 
-	i++;	
+i++;	
 	 }
+
 	 
 	
     if (message.content === '!radio') {
