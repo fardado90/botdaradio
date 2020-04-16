f1228f1c9e73fb64bda22fecefc6f86a7371f4bf
fardado90 <55768801+fardado90@users.noreply.github.com>
2020-04-16 17:38:28 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	 var carros = message.split(',');
+	 var carros = message.content.split(',');
 	 var i = 0;
 	 while (i < carros.length) {
     		message.channel.send(carros[i]);
